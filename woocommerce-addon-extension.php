51d3a4fb3adfd47443036099e39318f9de6b2589
Ronak Vanpariya <vanpariyar@gmail.com>
2020-11-08 12:20:09 +0530

    created fields

--- woocommerce-addon-extension.php
+++ woocommerce-addon-extension.php
@@ -45,6 +45,12 @@ class Wp_Wocommerce_Addon_Extension {
     
     function __construct(){
         add_action('plugins_loaded', array( $this,'check_some_other_plugin'), 10 );
+        add_action( 'woocommerce_register_form', array( $this,'misha_add_register_form_field'), 10 );
+        add_action( 'woocommerce_register_post', array( $this,'misha_validate_fields'), 10, 3 );  
+        add_action( 'woocommerce_created_customer', array( $this,'misha_save_register_fields') );
+        add_action( 'woocommerce_edit_account_form', array( $this,'misha_add_field_edit_account_form') );
+        add_action( 'woocommerce_save_account_details', array( $this,'misha_save_account_details') );
+        add_filter('woocommerce_save_account_details_required_fields', array( $this,'misha_make_field_required'));
     } 
     
     public function check_some_other_plugin() {
@@ -57,4 +63,92 @@ class Wp_Wocommerce_Addon_Extension {
 		}
     }
 
+    function misha_add_register_form_field(){
+    
+        woocommerce_form_field(
+            'country_to_visit',
+            array(
+                'type'        => 'text',
+                'required'    => true, // just adds an "*"
+                'label'       => 'Country you want to visit the most'
+            ),
+            ( isset($_POST['country_to_visit']) ? $_POST['country_to_visit'] : '' )
+        );
+        woocommerce_form_field(
+            'country_to_visit',
+            array(
+                'type'        => 'text',
+                'required'    => true, // just adds an "*"
+                'label'       => 'Country you want to visit the most'
+            ),
+            ( isset($_POST['country_to_visit']) ? $_POST['country_to_visit'] : '' )
+        );
+        woocommerce_form_field(
+            'country_to_visit',
+            array(
+                'type'        => 'text',
+                'required'    => true, // just adds an "*"
+                'label'       => 'Country you want to visit the most'
+            ),
+            ( isset($_POST['country_to_visit']) ? $_POST['country_to_visit'] : '' )
+        );
+    
+    }
+ 
+    function misha_validate_fields( $username, $email, $errors ) {
+    
+        if ( empty( $_POST['country_to_visit'] ) ) {
+            $errors->add( 'country_to_visit_error', 'We really want to know!' );
+        }
+    
+    }
+ 
+    function misha_save_register_fields( $customer_id ){
+    
+        if ( isset( $_POST['country_to_visit'] ) ) {
+            update_user_meta( $customer_id, 'country_to_visit', wc_clean( $_POST['country_to_visit'] ) );
+        }
+    
+    }
+
+    
+    // or add_action( 'woocommerce_edit_account_form_start', 'misha_add_field_edit_account_form' );
+    function misha_add_field_edit_account_form() {
+    
+        woocommerce_form_field(
+            'country_to_visit',
+            array(
+                'type'        => 'text',
+                'required'    => true, // remember, this doesn't make the field required, just adds an "*"
+                'label'       => 'Country you want to visit the most',
+                'description' => 'Maybe it is Norway or New Zealand or...?',
+            ),
+            get_user_meta( get_current_user_id(), 'country_to_visit', true ) // get the data
+        );
+    
+    }
+    
+    /**
+     * Step 2. Save field value
+     */
+    
+    function misha_save_account_details( $user_id ) {
+    
+        update_user_meta( $user_id, 'country_to_visit', sanitize_text_field( $_POST['country_to_visit'] ) );
+    
+    }
+    
+    /**
+     * Step 3. Make it required
+     */
+    
+    function misha_make_field_required( $required_fields ){
+    
+        $required_fields['country_to_visit'] = 'Country you want to visit the most';
+        return $required_fields;
+    
+    }
+
 }
+
+$wp_wocommerce_addon_extension = new Wp_Wocommerce_Addon_Extension();
