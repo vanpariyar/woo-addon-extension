3d750f6bfce6b5513faf9de94572d542e71db822
Ronak Vanpariya <vanpariyar@gmail.com>
2020-11-08 12:22:19 +0530

    changes in name

--- woo-addon-extension.php
+++ woo-addon-extension.php
@@ -2,15 +2,15 @@
 /**
  * Plugin Name
  *
- * @package           Wocommerce addon extension
+ * @package           Woo addon extension
  * @author            Ronak J Vanpariya
  * @copyright         Ronak J Vanpariya
  * @license           GPL-2.0-or-later
  *
  * @wordpress-plugin
- * Plugin Name:       Wocommerce addon extension
+ * Plugin Name:       Woo addon extension
  * Plugin URI:        https://github.com/vanpariyar/woocommerce-addon-extension
- * Description:       Wocommerce addon extension.
+ * Description:       Woo addon extension.
  * Version:           0.1
  * Requires at least: 5.0
  * Requires PHP:      5.0
@@ -20,10 +20,10 @@
  * License:           GPL v2 or later
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt 
  * 
- Wocommerce addon extension is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or any later version. 
- Wocommerce addon extension is distributed in the hope that it will be useful,but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ Woo addon extension is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or any later version. 
+ Woo addon extension is distributed in the hope that it will be useful,but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  
- You should have received a copy of the GNU General Public License along with Wocommerce addon extension. If not, see  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ You should have received a copy of the GNU General Public License along with Woo addon extension. If not, see  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
 */
 
 // Make sure we don't expose any info if called directly
@@ -41,7 +41,7 @@ if (!defined('WP_WOOCOMMERCE_ADDON_EXTENSION_FILE_PATH')) {
     define('WP_WOOCOMMERCE_ADDON_EXTENSION_FILE_PATH', plugin_dir_path(__FILE__));
 }
 
-class Wp_Wocommerce_Addon_Extension {
+class Wp_Woo_Addon_Extension {
     
     function __construct(){
         add_action('plugins_loaded', array( $this,'check_some_other_plugin'), 10 );
@@ -151,4 +151,4 @@ class Wp_Wocommerce_Addon_Extension {
 
 }
 
-$wp_wocommerce_addon_extension = new Wp_Wocommerce_Addon_Extension();
+$wp_woo_addon_extension = new Wp_Woo_Addon_Extension();
